b86477653c5e645ee05466f7a367a3edc6c16795
elithsar <joffrey.boucy@gmail.com>
2025-08-21 18:55:09 +0200

    Added buffered read

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import(
     "fmt"
+    "log"
+    "io"
+    "os"
 )
 
 func main() {
-    fmt.Println("I hope I get the job!")
+    file, err := os.Open("messages.txt")
+    if err != nil {
+	log.Fatal(err)
+    }
+
+    for {
+        bytes := make([]byte, 8)
+        _, err := file.Read(bytes)
+        
+	if err == io.EOF {
+	    break
+        } else if err != nil {
+            log.Fatal(err)
+        }
+	fmt.Println("read:", string(bytes))
+    }
+
 }
