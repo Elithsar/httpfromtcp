96082f06793f6a8ad8f5393602dce692f6621a32
elithsar <joffrey.boucy@gmail.com>
2025-08-21 19:22:56 +0200

    Added line buffer

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import(
     "log"
     "io"
     "os"
+    "strings"
 )
 
 func main() {
@@ -13,16 +14,22 @@ func main() {
 	log.Fatal(err)
     }
 
+    var line string
+    bytes := make([]byte, 8)
     for {
-        bytes := make([]byte, 8)
         _, err := file.Read(bytes)
-        
+
 	if err == io.EOF {
 	    break
         } else if err != nil {
             log.Fatal(err)
         }
-	fmt.Println("read:", string(bytes))
-    }
 
+	parts := strings.Split(string(bytes), "\n")
+	line += parts[0]
+	if len(parts) > 1 {
+            fmt.Println("read:", line)
+	    line = parts[1]
+        }
+    }
 }
